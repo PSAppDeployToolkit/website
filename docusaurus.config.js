22d9b65a220edf4bb5a0e3fc7749ca31ab3aa7d0
Dan Cunningham <dancunningham@me.com>
2024-12-01 13:51:00 -0500

    Cleanup work
    Start v4 docs buildout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,8 +20,9 @@ const config = {
   url: 'https://psappdeploytoolkit.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  deploymentBranch: 'gh-pages',
   baseUrl: '/website/',
-
+  trailingSlash: true,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PSAppDeployToolkit', // Usually your GitHub org/user name.
