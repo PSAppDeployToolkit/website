147fb148e8452bcc95cb886747150589ee9b0b58
Dan Gough <dan.gough@patchmypc.com>
2025-07-07 16:04:26 +0100

    Add 4.0 changes to sidebar

--- sidebars.js
+++ sidebars.js
@@ -23,7 +23,15 @@ const sidebars = {
         type: 'generated-index',
       },
       collapsed: false,
-      items: ['getting-started/licensing', 'getting-started/requirements', 'getting-started/download', 'getting-started/changes-in-v41', 'getting-started/creating-a-new-deployment', 'getting-started/migrating-from-v3', 'getting-started/faq'],
+      items: [
+        'getting-started/licensing',
+        'getting-started/requirements',
+        'getting-started/download',
+        'getting-started/changes-in-v41',
+        'getting-started/changes-in-v40',
+        'getting-started/creating-a-new-deployment',
+        'getting-started/migrating-from-v3',
+        'getting-started/faq'],
     },
     {
       type: 'category',
